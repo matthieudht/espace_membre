a25e566c104e2c0d4447ebeee98b3f895760d4d5
matthieudht <matthieu.dehondt@gmail.com>
2016-10-19 15:05:30 +0200

    formulaire_post.php: test if pseudo is available ok

--- formulaire_post.php
+++ formulaire_post.php
@@ -3,17 +3,17 @@
 //test if all field are present and not empty
 if ((isset($_POST['pseudo'], $_POST['password'], $_POST['verifpassword'], $_POST['email'])) && (!empty($_POST['pseudo']) && !empty($_POST['password'])&& !empty($_POST['verifpassword']) && !empty($_POST['email'])))
 {
-    echo 'the fields are ok';
+    echo 'the fields are ok'.'<br/>';
     foreach ($_POST as $key => $value)
     {
 	echo $key.' : '.$value."<br/>";
     };
-    echo empty($_POST['pseudo']);
-    echo empty($_POST['password']);
+    echo empty($_POST['pseudo']).'<br/>';
+    echo empty($_POST['password']).'<br/>';
     
 }
 else {
-    echo 'carefull, some fields are blank';
+    echo 'carefull, some fields are blank'.'<br/>';
 }
 
 
@@ -21,12 +21,30 @@ else {
 try
 {
     $mybase = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'dehondtmatthieu', 'mD120989');
-    echo 'connexion to espace_membre database is ok';
+    echo 'connexion to espace_membre database is ok'.'<br/>';
 }
 catch (exception $error)
 {
     die('error'.$error->getMessage());
 }
+
+
+// test if pseudo is not already taken in database
+$request = $mybase->prepare('SELECT * FROM Membres WHERE pseudo = ?');
+$request->execute(array($_POST['pseudo']));
+$result = $request->fetch();
+echo $result['pseudo'].'<br/>';
+
+if ($result)
+{
+    echo 'the pseudo is already taken'.'<br/>';
+}
+else
+{
+    echo 'good choice of pseudo'.'<br/>';
+}
+
+
 
     
 
