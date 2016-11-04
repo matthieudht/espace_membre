47295b44338e61945ad6430b38a48e55a513d86f
matthieudht <matthieu.dehondt@gmail.com>
2016-11-04 15:51:34 +0100

    Update connexion_sql.php

--- modele/connexion_sql.php
+++ modele/connexion_sql.php
@@ -4,7 +4,7 @@
 //connexion to database
 try
 {
-    $mybase = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'dehondtmatthieu', 'mD120989');
+    $mybase = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', );
     
 }
 catch (exception $error)
